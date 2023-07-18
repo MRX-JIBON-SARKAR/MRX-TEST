bb56fdec3499dd4e179be9794bf108b3f0a996d7
MRX-JIBON-SARKAR <138970072+MRX-JIBON-SARKAR@users.noreply.github.com>
2023-07-18 23:04:32 +0600

    Add files via upload

---
+++ logo.test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+print ('Tool Updated ')
+os.system("espeak \"Follow your facebook page\"")  
+os.system('xdg-open https://www.facebook.com/jibon.islam65653?mibextid=ZbWKwL/');time.sleep(1)
+print ('Tool Updated ')
+print ('Tool Updated ')
+print ('Tool Updated ')
+print ('Tool Updated ')
+print ('Tool Updated ')
+print ('Tool Updated ')
+print ('Tool Updated ')
+print ('Tool Updated ')
+print ('Tool Updated ')
+os.system('xdg-open https://www.facebook.com/groups/405527491346402/?ref=share')
+print (f"""
+     \33[33;1m██ \33[1;97m██ \33[33;1m██████ \33[1;97m██████ \33[33;1m███    ██  
+     \33[33;1m██ \33[1;97m██ \33[33;1m██   ██ \33[1;97m██████ \33[33;1m████   ██ 
+     \33[33;1m██ \33[1;97m██ \33[33;1m██████ \33[1;97m██    ██ \33[33;1m██ ██  ██ 
+\33[33;1m██   ██ \33[1;97m██ \33[33;1m██   ██ \33[1;97m██    ██ \33[33;1m██  ██ ██ 
+ \33[33;1m█████ \33[1;97m██ \33[33;1m██████ \33[1;97m██████ \33[33;1m██   ████ 
+""")
