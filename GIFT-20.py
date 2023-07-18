8367b81a13ac7aa7d2d12bf57c0dd5dc65c6604a
MRX-JIBON-SARKAR <138970072+MRX-JIBON-SARKAR@users.noreply.github.com>
2023-07-19 02:31:20 +0600

    Add files via upload

--- GIFT-20.py
+++ GIFT-20.py
@@ -9,7 +9,7 @@ import os,sys,time,json,random,re,string,platform,base64,platform,uuid
 #import requests,random,sys,json,os,re
 from time import sleep
 from os import system
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+#import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
 from datetime import date
 from datetime import datetime
 from time import sleep
