d8e7f7aab60ba98ffebd3cb91230f4defc5d99cd
MRX-JIBON-SARKAR <138970072+MRX-JIBON-SARKAR@users.noreply.github.com>
2023-07-19 02:29:42 +0600

    Add files via upload

--- GIFT-20.py
+++ GIFT-20.py
@@ -6,7 +6,7 @@
 from bs4 import BeautifulSoup as sop
 from concurrent.futures import ThreadPoolExecutor as tred
 import os,sys,time,json,random,re,string,platform,base64,platform,uuid
-import requests,random,sys,json,os,re
+#import requests,random,sys,json,os,re
 from time import sleep
 from os import system
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
